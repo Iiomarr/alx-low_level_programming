785da258cf4f49350d46705fe4118aa78d9e5871
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-10 15:05:28 -0700

    commity

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main -Entry point
+ *
+ * Description :A C programming priniting using puts function
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
