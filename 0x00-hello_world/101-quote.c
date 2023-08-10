b6b2437732eaed5e87304f5e46506f6cebda2efa
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-10 16:15:43 -0700

    commity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  *
  * Description : printing using write function
  *
- * Return :Always 0  (success)
+ * Return:Always 0  (success)
 */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1,quo,59);
+	write(1, quo, 59);
 	return (1);
 }
