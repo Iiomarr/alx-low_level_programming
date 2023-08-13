2e290def687d4bfd3809a765d6baad1c8e6b4cc4
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-13 14:18:49 -0700

    commity

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: all alphabets in lowercase using while
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+
+	return (0);
+}
+
