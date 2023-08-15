5c5866b8b22ecbc1b831c220c11b20baf8992b1e
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-15 14:59:35 -0700

    commity

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - printing absolute values
+ *
+ * @n: integer type
+ *
+ * Return: 0 (succes)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (0);
+}
