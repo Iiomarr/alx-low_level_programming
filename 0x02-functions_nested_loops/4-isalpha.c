c9d028c0128d6dab1204dff6d4ddd308f8384222
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-15 14:41:12 -0700

    commity

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - prints all alphabets
+ *
+ * @c: takes input from other functions
+ *
+ * Return: returns 1 is any kind of alphabets otherwise 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
