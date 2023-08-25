149bfdf70fb19d4f29847d460f6478a274d5a049
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-25 11:47:42 -0700

    commity

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - function for strings concat
+ * @src: parameter
+ * @dest: parameter
+ * Return: Always 0
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int c = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (c = 0; src[c]; c++)
+		dest[i++] = src[c++];
+	return (dest);
+}
