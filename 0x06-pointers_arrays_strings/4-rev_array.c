1f3fb1263076aff9980652ec4cffb3a58a1fc7ec
Iiomar <omar.kh.abdullatif@gmail.com>
2023-08-25 19:14:04 -0700

    commity

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function that array numbers
+ * @a: paramater for array
+ * @n: paramater for array count
+ * Return: Always 0.
+*/
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0; j = (n - 1); i < J; i++; j++)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
